0d42f7e2dc64d4911bb51a1bb880073864cd5a69
xale08 <48703832+xale08@users.noreply.github.com>
2022-02-07 00:28:30 -0600

    cambio

--- src/Testimonials.js
+++ src/Testimonials.js
@@ -5,7 +5,7 @@ import posed, { PoseGroup } from 'react-pose';
 const AnimatedContainer = posed.div({
   enter: {
     opacity: 1,
-    delay: 200
+    delay: 150
   },
   exit: {
     opacity: 0
@@ -23,7 +23,7 @@ const QuoteContainer = styled(AnimatedContainer)`
 const Container = styled.section`
   display: grid;
   grid-template-columns: 30px 1fr 30px;
-  height: 250px;
+  height: 100%;
   justify-items: center;
   box-shadow: 0 4px 6px hsla(0, 0%, 0%, 0.2);
   border-radius: 4px;
@@ -88,34 +88,29 @@ class Testimonials extends React.Component {
           'Muy fácil la realización de compra, muy atentos muchas gracias por el servicio 100% recomendado desde Tijuana, Baja California',
         client: 'Daniel'
       },
-      
 
       {
         message:
           'Las flores están hermosas y me encantó que avisaran por SMS cuando estaban por entregarlo. Sin duda volveré a pedir flores con ustedes. LOS MEJORES DE CD. OBREGÓN',
         client: 'Raul'
       },
-      
 
       {
         message:
           'Poder enviar flores desde Estados Unidos a México así de fácil fue padrísimo!!! Muchas gracias',
         client: 'Karen'
-      },
- 
-
-
+      }
     ],
     currentIndex: 0
   };
 
   previousQuote = () =>
-    this.setState(prevState => ({
+    this.setState((prevState) => ({
       currentIndex: (prevState.currentIndex + 1) % prevState.quotes.length
     }));
 
   nextQuote = () =>
-    this.setState(prevState => ({
+    this.setState((prevState) => ({
       currentIndex:
         (prevState.currentIndex - 1 + prevState.quotes.length) %
         prevState.quotes.length
